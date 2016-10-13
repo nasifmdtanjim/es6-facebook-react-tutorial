1b17f6957e9500b870343cab59b384a294f4ef62
nasif-md-tanjim-m4 <nasif.tanjim@metafour.com>
2016-10-13 19:45:24 +0600

    Comment list complete

--- src/app.js
+++ src/app.js
@@ -42,30 +42,106 @@ class CommentList extends React.Component {
 }
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: '',
+      text: ''
+    };
+
+    this.handleAuthorChange = (e) => {
+      this.setState({author: e.target.value});
+    };
+
+    this.handleTextChange = (e) => {
+      this.setState({text: e.target.value});
+    };
+
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      var author = this.state.author.trim();
+      var text = this.state.text.trim();
+      if (!text || !author)
+        return;
+
+      this.props.onCommentSubmit({author: author, text: text});
+      this.setState({author: '', text: ''});
+    };
+  }
+
   render() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange.bind(this)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange.bind(this)}
+        />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 }
 
 class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    // setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval.bind);
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: (data) => this.setState({data: data}),
+      error: (xhr, status, err) => console.error(this.props.url, status, err.toString())
+    });
+  }
+
+  handleCommentSubmit(comment) {
+    var comments = this.state.data;
+    comment.id = Date.now();
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: (data) => this.setState({data: data}),
+      error: (xhr, status, err) => {
+        this.setState({data: comments});
+        console.error(this.props.url, status, err.toString());
+      }
+    });
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit.bind(this)} />
       </div>
     );
   }
 }
 
-var data = [
-  {id: 1, author: 'Pete Hunt', text: 'This is one comment'},
-  {id: 2, author: 'Jordan Walke', text: 'This is *another* comment'}
-];
-
-ReactDOM.render(<CommentBox data={data} />, document.getElementById('content'));
+ReactDOM.render(<CommentBox url="/api/comments.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
